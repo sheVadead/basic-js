b6b65db8506d6c9c65403e180b1b10cc8e2ddf7d
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-09-18 09:51:16 +0300

    feat: add solution for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  
+    let defaultOptions = {
+        additionSeparator: options.additionSeparator,
+        separator: options.separator || '+',
+        repeatTimes: options.repeatTimes,
+        additionRepeatTimes: options.additionRepeatTimes,
+        addition: options.addition,
+    }
+    str = String(str)
+    let result = '';
+    if (options.addition===false || options.addition===null) {
+        defaultOptions.addition = String(options.addition)
+        
+    }
+    let methods = {
+        addition() {
+            if (!!defaultOptions.addition && !!defaultOptions.repeatTimes) {
+                if (defaultOptions.additionSeparator===undefined){
+                    result = (defaultOptions.addition.toString()).repeat(defaultOptions.additionRepeatTimes)
+                } else {
+                    result = (defaultOptions.addition.toString() + defaultOptions.additionSeparator).repeat(defaultOptions.additionRepeatTimes).slice(0, -defaultOptions.additionSeparator.length)
+                }
+                
+            } else if (!!defaultOptions.addition) (
+                result = defaultOptions.addition.toString()
+            )
+        }
+      
+    }
+   methods.addition();
+    return !!defaultOptions.repeatTimes?((str.toString() + result + defaultOptions.separator).repeat(defaultOptions.repeatTimes)).slice(0, -defaultOptions.separator.length):str.toString() + result
+}
+
   
