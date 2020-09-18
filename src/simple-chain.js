4807a71ca8a1311d2a40d86f250763ef41f776b0
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-09-18 09:50:01 +0300

    feat: add solution for chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
+const chainMaker= {
+  result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value){
+      if (value===undefined) {
+          value='( )';
+          this.result.push( value )
+      } else {
+          this.result.push('('+ ' '+ value + ' '+ ')' )
+      }
+      
+      return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if (position < 0 || position===undefined) {
+        this.result = [];
+          throw new Error
+      }
+      this.result.splice(position - 1,1);
+      return this;
   },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+ reverseChain () {
+  this.result.reverse()
+  return this
+ },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chain  = this.result.join('~~');
+     this.result=[];
+     return  chain;
   }
 };
 
