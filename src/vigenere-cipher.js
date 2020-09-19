d3abbe52403a7ea0b613a5043c3039f875026e7d
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-09-19 17:23:35 +0300

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode) {
+    if(mode===false || mode==='false') {
+      this.modification = 'reverse'
+    }
+    else {
+      this.modification = 'direct';
+    }
+  }
+  encrypt(msg, key) {
+    let answer=[];
+    let finalStr=''
+   let cleanWord = msg.toLowerCase().split('').filter(item=>item.charCodeAt()>=97 && item.charCodeAt()<=122)
+    let rightKey = (key.toLowerCase().repeat(Math.ceil(cleanWord.length/key.length))).slice(0, cleanWord.length)
+    cleanWord.map((item,index)=>{
+        (item.charCodeAt()-97) + (rightKey[index].charCodeAt()-97) >= 26 ? answer.push((item.charCodeAt()-97) + (rightKey[index].charCodeAt()-97) - 26 + 97):  answer.push((item.charCodeAt()-97) + (rightKey[index].charCodeAt()-97) + 97)
+    })
+    msg.split('').map((item,index)=>{((msg[index].charCodeAt()<65 && 31<msg[index].charCodeAt()) || msg[index].charCodeAt()===94 || msg[index].charCodeAt()===124) && answer.splice(index, 0, msg[index].charCodeAt())})
+    answer.map(item=>{finalStr+=String.fromCharCode(item )
+})
+return this.modification==='reverse'? finalStr.split('').reverse().join('').toUpperCase():finalStr.toUpperCase()
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(msg, key) {
+    let answer=[];
+    let finalStr=''
+   let cleanWord = msg.toLowerCase().split('').filter(item=>item.charCodeAt()>=97 && item.charCodeAt()<=122)
+    let rightKey = (key.toLowerCase().repeat(Math.ceil(cleanWord.length/key.length))).slice(0, cleanWord.length)
+    cleanWord.map((item,index)=>{
+        (item.charCodeAt()-97) - (rightKey[index].charCodeAt()-97) <0 ? answer.push((item.charCodeAt()-97) - (rightKey[index].charCodeAt()-97) + 26 + 97):  answer.push((item.charCodeAt()-97) - (rightKey[index].charCodeAt()-97) + 97)
+    })
+    msg.split('').map((item,index)=>{((msg[index].charCodeAt()<65 && 31<msg[index].charCodeAt()) || msg[index].charCodeAt()===94 || msg[index].charCodeAt()===124) && answer.splice(index, 0, msg[index].charCodeAt())})
+    answer.map(item=>{
+  
+      finalStr+=String.fromCharCode(item )
+})
+return this.modification==='reverse'? finalStr.split('').reverse().join('').toUpperCase():finalStr.toUpperCase();
   }
 }
 
